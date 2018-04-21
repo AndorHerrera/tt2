0f972999108583a08664f6d9198161cc9f669a55
Ángel <angel@MacBook-Air.local>
2018-04-21 11:04:40 -0500

    compras

--- src/app/buys/buys.component.ts
+++ src/app/buys/buys.component.ts
@@ -4,6 +4,9 @@ import { Profile } from 'app/_models/profile.model';
 import { BuysService } from './buys.service';
 import { Proyect } from '../_models/proyect.model';
 import { Buy } from '../_models/buy.model';
+import { ProyectDetailsService } from '../proyectdetails/proyectdetails.service';
+import { Folder } from '../_models/folder.model';
+import { Router } from '@angular/router';
 declare var $:any;
 
 declare interface TableData {
@@ -18,7 +21,7 @@ declare interface TableData {
 })
 export class BuysComponent implements OnInit {
 
-  constructor(private _buysService: BuysService) { }
+  constructor(private _buysService: BuysService, private _proyectDetailsService: ProyectDetailsService,private router:Router) { }
 
   profile:Profile=new Profile;
   idUsuario:string;
@@ -45,12 +48,39 @@ export class BuysComponent implements OnInit {
     }
   }
 
+  verDetalle(idProyecto:string) {
+    this._proyectDetailsService.getFolder(idProyecto).subscribe(response => {
+      let folder:Folder = response[0];
+      if(folder.id!=undefined){
+        this.router.navigate(['/proyectDetails',folder.id]);
+      }
+    });
+  }
+
+  verGraficas(idProyecto:string) {
+    this._proyectDetailsService.getFolder(idProyecto).subscribe(response => {
+      let folder:Folder = response[0];
+      if(folder.id!=undefined){
+        this.router.navigate(['/infoCharts',folder.id]);      
+      }
+    });
+  }
+
+  verIssues(idProyecto:string){
+    this._proyectDetailsService.getFolder(idProyecto).subscribe(response => {
+      let folder:Folder = response[0];
+      if(folder.id!=undefined){
+        this.router.navigate(['/issues',folder.id]);      
+      }
+    });
+  }
+
   cargaTabla(){
     setTimeout(function () {
       $('#myTable').DataTable({
         "aLengthMenu": [[10, 25, 100, -1], [10, 25, 100, "Todos"]],
         "iDisplayLength": 5,
-        "aoColumns": [{ "bSortable": false },{ "bSortable": true },{ "bSortable": true },{ "bSortable": true },{ "bSortable": false },{ "bSortable": false },{ "bSortable": false },{ "bSortable": false },{ "bSortable": false },{ "bSortable": false },{ "bSortable": false },{ "bSortable": true }],
+        "aoColumns": [{ "bSortable": false },{ "bSortable": true },{ "bSortable": true },{ "bSortable": true },{ "bSortable": true },{ "bSortable": false },{ "bSortable": false },{ "bSortable": false }],
           "oLanguage": {
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ ",
