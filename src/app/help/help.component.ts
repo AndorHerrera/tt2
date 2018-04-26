5a97e5f43ff6b9ff43f834c056a0b224f90947a3
pascaciorc <pascaciorc@gmail.com>
2018-04-26 12:55:45 -0500

    Tienda v2

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpComponent implements OnInit {
 
-  proys = [{title: '¿Qué es un software point?', url: 'esta', img:'h3.jpg'},
-  {title: 'Ayuda', url: 'esta', img:'h2.jpg'},
-  {title: 'Tutorial', url: 'esta', img:'h1.jpg'},
-  {title: 'Trabajo colaborativo', url: 'esta', img:'cg.jpg'},
-  {title: '¿Cómo calificamos los proyectos?', url: 'esta', img:'sq.png'}];
+  proys = [{title: '¿Qué es un software point?', url: 'https://www.youtube.com/watch?v=O-8IYWMltIg', img:'h3.jpg'},
+  {title: 'Ayuda', url: 'https://www.youtube.com/watch?v=EgFFAeG5BBQ', img:'h2.jpg'},
+  {title: 'Tutorial', url: 'https://www.youtube.com/watch?v=O-8IYWMltIg', img:'h1.jpg'},
+  {title: 'Trabajo colaborativo', url: 'https://www.youtube.com/watch?v=EgFFAeG5BBQ', img:'cg.jpg'},
+  {title: '¿Cómo calificamos los proyectos?', url: 'https://www.youtube.com/watch?v=O-8IYWMltIg', img:'sq.png'}];
 
   constructor() { }
 
--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -8,16 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class MarketComponent implements OnInit {
   //proys = ['UNO','DOS','TRES','CUATRO','CINCO'];
 
-  proys = [{id: '123', title: 'Proyecto web', desc: 'Un proyecto donde se desarrolla un sistema web en el los usuarios se pueden registrar en una red social', points: 300, img: 'c2.jpg', tags: ['Web', 'Red social'] },
+  data = [{id: '123', title: 'Proyecto web', desc: 'Un proyecto donde se desarrolla un sistema web en el los usuarios se pueden registrar en una red social', points: 300, img: 'c2.jpg', tags: ['Web', 'Red social'] },
+  {id: '746', title: 'Proyecto backend', desc: 'Un proyecto donde se modela una base de datos de usuarios de una biblioteca', points: 290, img: 'c1.jpg', tags: ['Base de datos', 'Java']},
     {id: '243', title: 'Proyecto mobil', desc: 'Un proyecto donde el usuario prodrá ver en un mapa su posición actual y en movimiento', points: 200, img: 'c3.jpeg', tags: ['Android', 'GPS', 'Google Maps']},
+    {id: '633', title: 'Proyecto java', desc: 'Un proyecto donde hacemos uso de las funciones de cadenas definidas para los objetos String', points: 235, img: 'c4.jpg', tags: ['Java']},
     {id: '633', title: 'Proyecto java', desc: 'Un proyecto donde hacemos uso de las funciones de cadenas definidas para los objetos String', points: 310, img: 'c4.jpg', tags: ['Java']},
-    {id: '746', title: 'Proyecto backend', desc: 'Un proyecto donde se modela una base de datos de usuarios de una biblioteca', points: 270, img: 'c1.jpg', tags: ['Base de datos', 'Java']},
+    {id: '746', title: 'Proyecto backend', desc: 'Un proyecto donde se modela una base de datos de usuarios de una biblioteca', points: 290, img: 'c1.jpg', tags: ['Base de datos', 'Java']},
+    {id: '653', title: 'Proyecto frontend', desc: 'Un proyecto donde se crea una pagina responsiva en la que se muestra un formulario', points: 430, img: 'hqdefault.jpg', tags: ['CSS', 'Bootstrap']},
+    {id: '123', title: 'Proyecto web', desc: 'Un proyecto donde se desarrolla un sistema web en el los usuarios se pueden registrar en una red social', points: 300, img: 'c2.jpg', tags: ['Web', 'Red social'] },
+    {id: '633', title: 'Proyecto java', desc: 'Un proyecto donde hacemos uso de las funciones de cadenas definidas para los objetos String', points: 450, img: 'c4.jpg', tags: ['Java']},
     {id: '653', title: 'Proyecto frontend', desc: 'Un proyecto donde se crea una pagina responsiva en la que se muestra un formulario', points: 430, img: 'hqdefault.jpg', tags: ['CSS', 'Bootstrap']}];
 
+  proys = this.data;
+  res = [];
+  
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  addTodo(term: string): void {
+    console.log(term);
+    this.res = [];
+    for(var i=0; i < this.data.length; i++) {
+      if(this.data[i].title.indexOf(term) >= 0){
+        console.log(this.data[i]);
+        this.res.push(this.data[i]);
+      }
+    }
+    this.proys = this.res;
   }
 
 }
